393c9df78cf767f253b68b089f621896f85b8eb4
Paushigaa <paushi213@gmail.com>
2023-03-06 10:35:46 +0530

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.admindashboard.app',
+  appName: 'admin-dashboard',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
